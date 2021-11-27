4f9f2289eb84fc0f7b0efdcbb231f46351781fd3
ahmed sobh <ahmedsobhms@outlook.com>
2021-11-27 15:10:04 +0200

    22

--- Khdoum.UI/Helpers/Constant.cs
+++ Khdoum.UI/Helpers/Constant.cs
@@ -7,7 +7,7 @@ namespace Khdoum.UI.Helpers
 {
     public static class Constant
     {
-        public static string BaseAddress { get; set; } = "https://localhost:44395/";
-       //public static string BaseAddress { get; set; } = "http://khdoumghtyherf1-001-site1.itempurl.com/";
+       // public static string BaseAddress { get; set; } = "https://localhost:44395/";
+       public static string BaseAddress { get; set; } = "http://khdoumghtyherf1-001-site1.itempurl.com/";
     }
 }
